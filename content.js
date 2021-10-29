69966badeaf422956bf59e7ad9620c8abcf1bdb9
sar1991 <abdirimov.1991@mail.ru>
2021-10-29 18:38:55 +0500

    gilamalar

--- content.js
+++ content.js
@@ -1,3 +1,8 @@
-document.querySelector('.accordion__btn').addEventListener('click', function () {
-  document.querySelector('.accorion__content').classList.toggle("active")
-})
+const acardionBtns = document.getElementsByClassName("questions__item-btn");
+
+for (let i=0; i < acardionBtns.length; i++) {
+  acardionBtns[i].addEventListener("click", function (e) {
+    e.target.closest(".questions__item-btn").classList.toggle("questions__item-btn--active")
+    e.target.closest(".questions__item-btn").nextElementSibling.classList.toggle("active");
+  })
+}
